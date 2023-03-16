0370ff75eac1345e03a1c28c5ca969999ba5e755
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-15 20:49:49 -0700

    installed express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const { EventEmitter } = require("event");
+const eventEmitter = new EventEmitter();
+eventEmitter.on("lunch", () => {
+  console.log("hello world");
+});
+
+eventEmitter.emit("lunch");
