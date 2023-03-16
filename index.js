89750dcc7d42faa0b73d94c9143eb2e4e84bdfcd
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-15 21:29:27 -0700

    users routes configured

--- index.js
+++
@@ -1,7 +0,0 @@
-const { EventEmitter } = require("event");
-const eventEmitter = new EventEmitter();
-eventEmitter.on("lunch", () => {
-  console.log("hello world");
-});
-
-eventEmitter.emit("lunch");
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("hello world");
   res.render("index");
 });
+
+const userRouter = require("./routes/users");
+
+app.use("/users/", userRouter);
 
 app.listen(3001);
